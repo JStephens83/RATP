b4580d0b6d20360ab3314b68af73c4ebe309a52f
Jeremy <35834716+JStephens83@users.noreply.github.com>
2025-06-03 12:25:30 +0200

    Visuels (#3)
    * travail sur background
    
    * Ajout liste lignes = couleurs + dispositions
    
    * Travail sur l'UX/UI

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <p class="logo">Projet RATP (Last call / Last train)</p>
-  </div>
-  <h1>🚆 Horaires des derniers trains RATP</h1>
+  <h1>Horaires des derniers trains RATP</h1>
   <Homeview />
 </template>
 
@@ -11,19 +8,10 @@
 </script>
 
 <style scoped>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-  }
   h1 {
-    margin-top: 10px;
+    display: block;
+    text-align: center;
+    font-size: 3em;
+    margin: 10vh;
   }
 </style>
--- src/components/LineSelector.vue
+++ src/components/LineSelector.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
     <div v-if="lignes.length > 0">
-      <label for="line">Sélectionnez une ligne :</label>
-    <select 
+      <h2 class="list-labels">Sélectionnez une ligne</h2>
+      <ul class="line-list">
+        <li
+          v-for="ligne in lignes"
+          :key="ligne.id"
+          :class="{ selected: selectedLine && selectedLine.id === ligne.id }"
+          @click="selectLine(ligne)"
+          :style="{ backgroundColor: ligne.color, color: ligne.textColor }"
+        >
+          {{ ligne.name }}
+        </li>
+      </ul>
+    <!-- <select 
       id="line" 
       v-model="selectedLine" 
       @change="handleLineChange"
@@ -10,10 +21,14 @@
       <option v-for="ligne in lignes" :key="ligne.id" :value="ligne">
         {{ ligne.name }}
       </option>
-    </select>
+    </select> -->
     </div>
     <div v-else>
-      <p>Chargement des lignes...</p>
+      <p class="loading"
+        :style="selectedLine 
+        ? { backgroundColor: selectedLine.color, color: selectedLine.textColor } 
+        : {}"
+      >Chargement des lignes...</p>
     </div>
   </div>
 </template>
@@ -29,9 +44,15 @@
   // Vérification de l'emit:
   const emit = defineEmits(["lineSelected"]);
 
-  const handleLineChange = () => {
-    console.log("Valeur de selectedLine dans LineSelector :", selectedLine.value);
-    emit("lineSelected", selectedLine.value); // Emit de la valeur sélectionnée
+  // va avec le select/option:
+  // const handleLineChange = () => {
+  //   console.log("Valeur de selectedLine dans LineSelector :", selectedLine.value);
+  //   emit("lineSelected", selectedLine.value); // Emit de la valeur sélectionnée
+  // };
+
+  const selectLine = (ligne) => {
+    selectedLine.value = ligne;
+    emit("lineSelected", ligne);
   };
 
   onMounted(async () => {
@@ -39,3 +60,27 @@
     console.log("Lignes chargées :", lignes.value);
   });
 </script>
+
+<style scoped>
+  ul.line-list {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+  ul.line-list>li {
+    display: flex;
+    border-radius: 50%;
+    cursor: pointer;  
+    width: 5vw;
+    height: 5vw;
+    align-items: center;
+    justify-content: center;
+    font-size: 2.1rem;
+    font-weight: bold;
+    transition: 0.3s;
+  }
+  ul.line-list>li:hover {
+    /* Ajouter animation */
+    box-shadow: 2px 2px 2px #000;
+  }
+</style>
